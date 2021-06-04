804b3db0b2dc2684d0a93c09e92dd5a770721842
Sergio10RPR <chejoperez.com10@gmail.com>
2021-06-04 13:28:44 -0600

    Funcionalidad menu

---
+++ menu.py
@@ -0,0 +1,26 @@
+def menu():
+    num=0
+    while num!=3:
+        print("*****MENU*****")
+        print("1.SUMA")
+        print("2.RESTA")
+        print("3.SALIR")
+        num=int(input("Ingrese la opcion: "))
+        print(num)
+        if num==1:
+            print("--Suma--")
+            print("Ingrese un numero: ")
+            a=int(input(": "))
+            print()
+            print("Ingrese un numero: ")
+            b=int(input(": "))
+            
+      
+        elif num==2:
+            print("--Resta--")
+            print("Ingrese un numero: ")
+            a=int(input(": "))
+            print()
+            print("Ingrese un numero: ")
+            b=int(input(": "))
+            
