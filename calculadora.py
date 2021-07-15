f7e2270f2f0428125056a7166cfd7a37d8593955
Rivelino Riverea <chejoperez.com10@gmail.com>
2021-07-15 17:12:29 -0600

    -m
    "Se agregó la multiplicacion y division"

--- calculadora.py
+++ calculadora.py
@@ -3,3 +3,12 @@ def suma(a,b):
 
 def resta(a,b):
     return a-b
+
+def multiplicar(a,b):
+    return a*b
+
+def dividir(a,b):
+    if b==0:
+        print('No se puede dividir dentro de 0')
+    else:
+        return a/b
--- menu.py
+++ menu.py
@@ -1,11 +1,13 @@
-from calculadora import resta,suma
+from calculadora import dividir, multiplicar, resta,suma
 def menu():
     num=0
-    while num!=3:
+    while num!=5:
         print("*****MENU*****")
         print("1.SUMA")
         print("2.RESTA")
-        print("3.SALIR")
+        print("3.MULTIPLICACION")
+        print("4.DIVISION")
+        print("5.SALIR")
         num=int(input("Ingrese la opcion: "))
         print(num)
         if num==1:
@@ -25,5 +27,19 @@ def menu():
             print("Ingrese un numero: ")
             b=int(input(": "))
             print("El resultado es: " , (resta(a,b)))
-            
+        elif num==3:
+            print("--Multiplicacion")
+            print("Ingrese un numero: ")
+            a=int(input(": "))
+            print("Ingrese un numero: ")
+            b=int(input(": "))
+            print("El resultado es: ",multiplicar(a,b))
+        elif num==4:
+            print("--Division")
+            print("Ingrese un numero: ")
+            a=int(input(": "))
+            print("Ingrese un numero: ")
+            b=int(input(": "))
+            print("El resultado es: ",dividir(a,b))
+
             
